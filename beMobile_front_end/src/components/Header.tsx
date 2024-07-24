74fdabe1bce9bc9a7679b264fd2495171ea401ec
Victor Farias <victor.calidor@gmail.com>
2024-07-24 18:03:29 -0300

    fixing indentation Header component

--- beMobile_front_end/src/components/Header.tsx
+++ beMobile_front_end/src/components/Header.tsx
@@ -1,12 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Image, Navbar } from 'react-bootstrap';
-import logoHeader from '../assets/logoHeader.png';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import logoHeader from "../assets/logoHeader.png";
 
 export const Header = () => {
-
   return (
-    <Navbar sticky="top" bg='white' style={{padding: "13px"}} className='shadow -3-strong mb-5'>
-      <Image src={logoHeader} height={"34"} style={{marginLeft: "20px"}}></Image>
-    </Navbar> 
-  )
-}
+    <Navbar
+      sticky="top"
+      bg="white"
+      style={{ padding: "13px" }}
+      className="shadow -3-strong mb-4"
+    >
+      <Image
+        src={logoHeader}
+        height={"34"}
+        style={{ marginLeft: "20px" }}
+      ></Image>
+    </Navbar>
+  );
+};
